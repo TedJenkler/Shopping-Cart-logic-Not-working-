13a133c2fb8633e7b3fcb6aa605b1b5d14498367
Teodor Jenkler <108183860+TedJenkler@users.noreply.github.com>
2022-09-03 12:49:38 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/counter";
+import Counters from "./components/Counters";
+
+function App() {
+  return (
+    <div>
+      <Counters />
+    </div>
+  );
+}
+
+export default App;
